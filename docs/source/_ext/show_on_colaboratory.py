d0e334a5250886e1ba022e0e19f25065b42d613a
keisuke-umezawa <keisuke.umezawa@gmail.com>
2018-07-17 19:21:52 +0900

    Add colab botton

---
+++ docs/source/_ext/show_on_colaboratory.py
@@ -0,0 +1,31 @@
+"""
+Sphinx extension to add ReadTheDocs-style "Edit on GitHub" links to the
+sidebar.
+Loosely based on https://github.com/astropy/astropy/pull/347
+"""
+
+import os
+import warnings
+
+
+__licence__ = 'BSD (3 clause)'
+
+
+def get_colaboratory_url(path):
+    return 'https://colab.research.google.com/github/chainer-community/chainer-colab-notebook/blob/master/{path}'.format(
+        path=path)
+
+
+def html_page_context(app, pagename, templatename, context, doctree):
+    if templatename != 'page.html':
+        return
+
+    path = os.path.relpath(doctree.get('source'), app.builder.srcdir).replace('notebook/', '')
+    show_url = get_colaboratory_url(path)
+
+    context['show_on_colaboratory_url'] = show_url
+    print(show_url)
+
+
+def setup(app):
+    app.connect('html-page-context', html_page_context)
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,9 +17,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('_ext'))
 
 
 # -- General configuration ------------------------------------------------
@@ -35,7 +35,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
-    'nbsphinx']
+    'nbsphinx',
+    'show_on_colaboratory',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
