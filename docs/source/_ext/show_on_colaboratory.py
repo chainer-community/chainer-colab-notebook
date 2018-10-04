30a6d3ff4dd655ed79db59a57eb6216b7dedf54f
keisuke umezawa <keisuke-umezawa@users.noreply.github.com>
2018-10-05 00:27:35 +0900

    Update show_on_colaboratory.py

--- docs/source/_ext/show_on_colaboratory.py
+++ docs/source/_ext/show_on_colaboratory.py
@@ -14,7 +14,7 @@ __licence__ = 'BSD (3 clause)'
 def get_colaboratory_url(path):
     fromto = [('hands_on', 'hands_on_en'), ('official_example', 'official_example_en')]
     for f, t in fromto:
-        if path.startwith(f):
+        if path.startswith(f):
             path = path.replace(f, t, 1)
     return 'https://colab.research.google.com/github/chainer-community/chainer-colab-notebook/blob/master/{path}'.format(
         path=path)
@@ -22,7 +22,7 @@ def get_colaboratory_url(path):
 
 def html_page_context(app, pagename, templatename, context, doctree):
     path = os.path.relpath(doctree.get('source'), app.builder.srcdir).replace('notebook/', '')
-    show_url = get_colaboratory_url(str(path))
+    show_url = get_colaboratory_url(path)
 
     context['show_on_colaboratory_url'] = show_url
 
